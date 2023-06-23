495c3ad0a42ff50554de825bffae13bc862ebaa5
snorkungen <teuvo2004@gmail.com>
2023-06-23 22:30:35 +0300

    feat: IPV4_ADDRESS

---
+++ src/lib/address/ipv4/ipv4.ts
@@ -0,0 +1,43 @@
+import { BaseAddress, defineAddress } from "../address";
+
+const DOT_NOTATED_ADDRESS_REGEX = /^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/;
+
+export class IPV4Address implements BaseAddress {
+    static ADDRESS_LENGTH: number = 32;
+    static parse(input: string): Buffer {
+        input = input.trim();
+        if (!DOT_NOTATED_ADDRESS_REGEX.test(input)) {
+            throw new Error("failed to parse: " + IPV4Address.name);
+        }
+
+        let buffer = Buffer.alloc(IPV4Address.ADDRESS_LENGTH / 8);
+
+        input.split(".").forEach((n, i) => {
+            buffer[i] = parseInt(n, 10);
+        })
+
+        return buffer;
+    }
+
+    buffer: Buffer;
+    constructor(input: string);
+    constructor(input: Buffer);
+    constructor(input: IPV4Address);
+    constructor(input: unknown) {
+        if (typeof input == "string") {
+            this.buffer = IPV4Address.parse(input)
+        } else if (input instanceof IPV4Address) {
+            this.buffer = Buffer.from(input.buffer);
+        } else if (input instanceof Uint8Array && (input.length * 8) == IPV4Address.ADDRESS_LENGTH) {
+            this.buffer = Buffer.from(input);
+        } else {
+            throw new Error("failed to initialize: " + IPV4Address.name)
+        }
+    }
+
+    toString(): string {
+        return `${this.buffer[0]}.${this.buffer[1]}.${this.buffer[2]}.${this.buffer[3]}`;
+    }
+}
+
+export const IPV4_ADDRESS = defineAddress(IPV4Address);
