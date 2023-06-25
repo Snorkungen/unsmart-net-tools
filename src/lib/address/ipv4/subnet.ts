bd9f8a07f4c5955aa3bb8baf3ee0c5ca6363bdf9
snorkungen <teuvo2004@gmail.com>
2023-06-25 13:47:29 +0300

    feat: address/ipv4/subnet.ts 'calculate subnet'

---
+++ src/lib/address/ipv4/subnet.ts
@@ -0,0 +1,32 @@
+import { xor, not } from "../../binary/buffer-bitwise";
+import { IPV4Address } from "../ipv4";
+import { AddressMask } from "../mask";
+
+export function calculateSubnetIPV4<Address extends typeof IPV4Address>(address: InstanceType<Address>, aMask: AddressMask<Address>) {
+    let networkAddress = aMask.mask(address),
+        broadcastAddress = new IPV4Address(
+            xor(networkAddress.buffer, not(aMask.buffer))
+        );
+
+    let minHostAddress = new IPV4Address(
+        networkAddress
+    ), maxHostAddress = new IPV4Address(
+        broadcastAddress
+    );
+
+    minHostAddress.buffer[3] = minHostAddress.buffer[3] | 1;
+    maxHostAddress.buffer[3] = maxHostAddress.buffer[3] ^ 1;
+
+    return {
+        address: new IPV4Address(address),
+        mask: aMask,
+        networkAddress: networkAddress,
+        broadcastAddress: broadcastAddress,
+
+        hosts: {
+            count: 2 ** (IPV4Address.ADDRESS_LENGTH - aMask.length) - 2,
+            min: minHostAddress,
+            max: maxHostAddress
+        }
+    }
+}
--- src/tests/address/ipv4.test.ts
+++ src/tests/address/ipv4.test.ts
@@ -1,7 +1,8 @@
 import { describe, test, expect } from "vitest";
-import { IPV4Address } from "../../lib/address/ipv4";
+import { IPV4Address, calculateSubnetIPV4 } from "../../lib/address/ipv4";
+import { createMask } from "../../lib/address/mask";
 
-describe("MAC Address", () => {
+describe("IPV4 Address", () => {
     test("toString", () => {
         let addr = "9.0.0.2";
         let buf = Buffer.from("09000002", "hex");
@@ -11,5 +12,41 @@ describe("MAC Address", () => {
         let addr = "192.168.30.179";
         let mac = new IPV4Address(addr);
         expect(mac.toString() == addr)
+    })
+
+    describe("Calculate Subnet", () => {
+        test("Calculate Subnet #1", () => {
+            let address = new IPV4Address("192.168.0.1"),
+                mask = createMask(IPV4Address, 24, false);
+
+            let subnet = calculateSubnetIPV4(address, mask);
+
+            expect(address).not.eq(subnet.address)
+            expect(address.toString()).eq(subnet.address.toString());
+
+            expect(subnet.networkAddress.toString()).eq("192.168.0.0")
+            expect(subnet.broadcastAddress.toString()).eq("192.168.0.255")
+
+            expect(subnet.hosts.count).eq(254);
+            expect(subnet.hosts.min.toString()).eq("192.168.0.1");
+            expect(subnet.hosts.max.toString()).eq("192.168.0.254");
+        })
+
+        test("Calculate Subnet #2", () => {
+            let address = new IPV4Address("192.168.0.40"),
+                mask = createMask(IPV4Address, 30, false);
+
+            let subnet = calculateSubnetIPV4(address, mask);
+
+            expect(address).not.eq(subnet.address)
+            expect(address.toString()).eq(subnet.address.toString());
+
+            expect(subnet.networkAddress.toString()).eq("192.168.0.40")
+            expect(subnet.broadcastAddress.toString()).eq("192.168.0.43")
+
+            expect(subnet.hosts.count).eq(2);
+            expect(subnet.hosts.min.toString()).eq("192.168.0.41");
+            expect(subnet.hosts.max.toString()).eq("192.168.0.42");
+        })
     })
 })
