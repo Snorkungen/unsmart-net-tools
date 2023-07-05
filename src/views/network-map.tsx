adc9d475da56f9bb8b8122e569051fa3715cc330
snorkungen <teuvo2004@gamil.com>
2023-07-06 00:23:49 +0300

    this is garbage

--- src/views/network-map.tsx
+++ src/views/network-map.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { Accessor, Component, For, createSignal } from "solid-js";
 import { JSX } from "solid-js/jsx-runtime";
 import { IPV4Address } from "../lib/address/ipv4";
 import { createMask } from "../lib/address/mask";
@@ -6,6 +6,98 @@ import { Host } from "../lib/device/host";
 import { Device } from "../lib/device/device";
 import { NetworkSwitch } from "../lib/device/network-switch";
 import { Interface } from "../lib/device/interface";
+
+let devices: NetworkMapDevice[] = [];
+let ifaceConnections: { master: NetworkMapInterface; slave: NetworkMapInterface; path: JSX.Element }[] = [];
+
+class NetworkMapInterface {
+    iface: Interface;
+    nmDevice: NetworkMapDevice;
+
+    i: number;
+
+    x: number;
+    y: number;
+
+    connectorStroke = "#33e42d"
+    connectorStrokeHighlight = "#cc2e12"
+
+    update() {
+        this.x = this.nmDevice.x + (this.i * 14);
+        this.y = this.nmDevice.y + 50;
+
+    }
+
+    constructor(nmDevice: NetworkMapDevice, iface: Interface, i: number) {
+        this.nmDevice = nmDevice;
+        this.iface = iface;
+        this.i = i;
+
+        this.x = this.nmDevice.x + (this.i * 14);
+        this.y = this.nmDevice.y + 50;
+    }
+
+    render() {
+        this.x = this.nmDevice.x + (this.i * 14);
+        this.y = this.nmDevice.y + 50;
+
+        let width = 10, height = width;
+
+
+        return <rect x={this.x} y={this.y} width={width} height={height} fill={this.iface.isConnected ? "green" : "red"} />
+    }
+}
+class NetworkMapDevice {
+    x: number;
+    y: number;
+    device: Device;
+    nmInterfaces: Array<NetworkMapInterface>
+    constructor(x: number, y: number, device: Device) {
+        this.x = x;
+        this.y = y;
+        this.device = device;
+
+        this.nmInterfaces = this.device.interfaces.map((iface, i) => new NetworkMapInterface(this, iface, i));
+
+        devices.push(this)
+    }
+
+    mouseIsDown = false;
+    mouseDownPos?: { x: number; y: number }
+
+    map?: NetworkMap;
+    update() {
+        if (!this.map) return;
+        this.map.update()
+    }
+    render() {
+        return <g onMouseDown={(ev) => {
+            this.mouseIsDown = true;
+            this.mouseDownPos = { x: ev.clientX, y: ev.clientY }
+        }}
+            onMouseMove={(ev) => {
+                if (!this.mouseIsDown || !this.mouseDownPos) return;
+
+                let diffX = ev.clientX - this.mouseDownPos.x, diffY = ev.clientY - this.mouseDownPos.y;
+
+                this.x += diffX;
+                this.y += diffY;
+
+                this.mouseDownPos.x = ev.clientX;
+                this.mouseDownPos.y = ev.clientY;
+
+                this.update()
+            }}
+            onMouseUp={() => this.mouseIsDown = false}
+            onMouseLeave={() => this.mouseIsDown = false}
+        >
+            <rect x={this.x} y={this.y} width={50} height={50} fill="#4f3f3f" />
+            <text x={this.x + 10} y={this.y + 20}>{this.device.name}</text>
+            {this.nmInterfaces.map(iface => iface.render())}
+        </g>
+    }
+
+}
 
 let networkSwitch = new NetworkSwitch();
 networkSwitch.name = "SW1"
@@ -33,137 +125,152 @@ swIface_pc1.connect(iface_pc1);
 swIface_pc2.connect(iface_pc2);
 swIface_pc3.connect(iface_pc3);
 
-export default function NetworkMap(): JSX.Element {
-    let nmDevice_sw = new NetworkMapDevice(120, 200, networkSwitch);
-    let nmDevice_pc1 = new NetworkMapDevice(100, 100, pc1);
-    let nmDevice_pc2 = new NetworkMapDevice(200, 200, pc2);
-    let nmDevice_pc3 = new NetworkMapDevice(300, 200, pc3);
+let nmDevice_sw = new NetworkMapDevice(120, 200, networkSwitch);
+let nmDevice_pc1 = new NetworkMapDevice(100, 100, pc1);
+let nmDevice_pc2 = new NetworkMapDevice(200, 200, pc2);
+let nmDevice_pc3 = new NetworkMapDevice(300, 200, pc3);
 
+export default function NetworkMapViewer(): JSX.Element {
 
-    return <div style={{ width: "100%" }}>
+    let nmap = new NetworkMap();
+    nmap.addDevice(nmDevice_pc1)
+    nmap.addDevice(nmDevice_pc2)
+    nmap.addDevice(nmDevice_pc3)
+    nmap.addDevice(nmDevice_sw)
+
+    return <div style={{ width: "100%" }} >
 
         <svg width={"100%"} height={500} >
-            {nmDevice_pc1.render()}
-            {nmDevice_pc2.render()}
-            {nmDevice_pc3.render()}
-            {nmDevice_sw.render()}
+            <g ref={(el => { nmap.container = el; nmap.update() })}></g>
         </svg>
 
     </div>
 };
 
-let devices: NetworkMapDevice[] = [];
+interface NetworkMapConnection {
+    master: NetworkMapInterface;
+    slave: NetworkMapInterface;
+    path: JSX.Element;
+}
+class NetworkMap {
+    devices: Array<NetworkMapDevice> = [];
+    connections: Array<NetworkMapConnection> = [];
 
-class NetworkMapInterface {
-    iface: Interface;
-    nmDevice: NetworkMapDevice;
+    map: JSX.Element;
 
-    i: number;
+    container?: Element;
 
-    x: number;
-    y: number;
-
-    connectorStroke = "#33e42d"
-    connectorStrokeHighlight = "#cc2e12"
-
-    constructor(nmDevice: NetworkMapDevice, iface: Interface, i: number) {
-        this.nmDevice = nmDevice;
-        this.iface = iface;
-        this.i = i;
-
-        this.x = this.nmDevice.x + (this.i * 14);
-        this.y = this.nmDevice.y + 50;
+    constructor() {
+        this.update()
     }
 
-    private findTarget(): NetworkMapInterface | null {
-        if (!this.iface.isConnected) return null;
+    private findTarget(iface: NetworkMapInterface): NetworkMapInterface | null {
+        if (!iface.iface.isConnected) return null;
 
-        for (let nmd of devices) {
-            if (nmd == this.nmDevice) continue;
-            let nmi = nmd.nmInterfaces.find((nmi) => nmi.iface == (this.iface as any).target)
+        for (let nmd of this.devices) {
+            if (nmd == iface.nmDevice) continue;
+            let nmi = nmd.nmInterfaces.find((nmi) => nmi.iface == (iface.iface as any).target)
 
             if (nmi) return nmi;
         }
         return null;
     }
 
-    render() {
-        this.x = this.nmDevice.x + (this.i * 14);
-        this.y = this.nmDevice.y + 50;
-
-        let path: any = null;
-        let target = this.findTarget()
-
+    calculateConnectionPath(master: NetworkMapInterface, slave: NetworkMapInterface): string {
         let width = 10, height = width;
 
-        pathsetter: if (target) {
-            let connectionMaster = (() => {
-                if (this.nmDevice.nmInterfaces.length != target.nmDevice.nmInterfaces.length) return this.nmDevice.nmInterfaces.length > target.nmDevice.nmInterfaces.length;
-                else return devices.indexOf(this.nmDevice) < devices.indexOf(target.nmDevice)
-            })()
-            if (!connectionMaster) break pathsetter;
+        let xStartOffset = width / 2;
+        let yPad = 15 * (master.i + 1);
 
-            let xStartOffset = width / 2;
-            let yPad = 15 * (this.i + 1);
+        let tx = slave.x, ty = slave.y;
 
-            let tx = target.x, ty = target.y;
+        let d = `M${master.x + xStartOffset} ${master.y + height}`;
 
-            let d = `M${this.x + xStartOffset} ${this.y + height}`;
-
-            if (this.y >= ty) {
-                d += `v${yPad}`
-            } else {
-                d += `v${yPad + (ty - this.y)}`
-            }
-
-            if (this.x == tx) {
-
-            } else {
-                d += `h${tx - this.x}`
-            }
-
-            if (this.y < ty) {
-                d += `v${-yPad}`
-            } else {
-                d += `v${ty - this.y - yPad}`
-            }
-
-            path = <path d={d} stroke={this.connectorStroke} stroke-width={width / 2} fill="none"
-                stroke-linejoin="round"
-                onMouseEnter={(e) => e.currentTarget.style.stroke = this.connectorStrokeHighlight}
-                onMouseLeave={(e) => e.currentTarget.style.stroke = this.connectorStroke}
-            />
+        if (master.y >= ty) {
+            d += `v${yPad}`
+        } else {
+            d += `v${yPad + (ty - master.y)}`
         }
 
-        return <g>
-            <rect x={this.x} y={this.y} width={width} height={height} fill={this.iface.isConnected ? "green" : "red"} />
-            {path}
-        </g>
-    }
-}
-class NetworkMapDevice {
-    x: number;
-    y: number;
-    device: Device;
-    nmInterfaces: Array<NetworkMapInterface>
-    constructor(x: number, y: number, device: Device) {
-        this.x = x;
-        this.y = y;
-        this.device = device;
+        if (master.x == tx) {
 
-        this.nmInterfaces = this.device.interfaces.map((iface, i) => new NetworkMapInterface(this, iface, i));
+        } else {
+            d += `h${tx - master.x}`
+        }
 
-        devices.push(this)
+        if (master.y < ty) {
+            d += `v${-yPad}`
+        } else {
+            d += `v${ty - master.y - yPad}`
+        }
+
+        return d;
     }
 
+    private calculateConnections() {
+        this.connections = [];
 
-    render() {
+        let touched: Array<NetworkMapInterface> = [];
+
+        for (let device of this.devices) {
+            for (let iface of device.nmInterfaces) {
+                if (!iface.iface.isConnected) continue;
+                let target = this.findTarget(iface);
+
+                if (!target) continue;
+                if (touched.indexOf(target) >= 0) continue;
+                let connectionMaster = (() => {
+                    if (device.nmInterfaces.length != target.nmDevice.nmInterfaces.length) return device.nmInterfaces.length > target.nmDevice.nmInterfaces.length;
+                    else return this.devices.indexOf(device) < this.devices.indexOf(target.nmDevice)
+                })()
+
+                if (!connectionMaster) continue;
+                this.connections.push({
+                    master: iface,
+                    slave: target,
+                    path: <path d={this.calculateConnectionPath(iface, target)} stroke={iface.connectorStroke} stroke-width={2} fill="none"
+                        stroke-linejoin="round"
+                        onMouseEnter={(e) => e.currentTarget.style.stroke = iface.connectorStrokeHighlight}
+                        onMouseLeave={(e) => e.currentTarget.style.stroke = iface.connectorStroke}
+                    />
+                })
+
+                touched.push(iface);
+            }
+        }
+
+
+    }
+
+    addDevice(device: NetworkMapDevice) {
+        device.map = this
+        this.devices.push(device);
+        this.update()
+    }
+
+    removeDevice(device: NetworkMapDevice) {
+        this.devices = this.devices.filter((d) => d != device);
+        this.update()
+    }
+
+    update() {
+        this.calculateConnections();
+
+        if (this.container) {
+            this.container.innerHTML = ""
+            this.container.appendChild(this.render() as any)
+        }
+
+    }
+
+    render(): JSX.Element {
         return <g>
-            <rect onDragStart={() => {
-                console.log("hdjkf")
-            }} x={this.x} y={this.y} width={50} height={50} fill="#4f3f3f" />
-            <text x={this.x + 10} y={this.y + 20}>{this.device.name}</text>
-            {this.nmInterfaces.map(iface => iface.render())}
+            <g>
+                {this.devices.map(d => d.render())}
+            </g>
+            <g>
+                {this.connections.map(c => c.path)}
+            </g>
         </g>
     }
 }
