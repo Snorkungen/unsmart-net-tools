c72ae855b13bdc6c81c5affa90b00611c2b28bb2
snorkungen <teuvo2004@gamil.com>
2023-07-19 20:04:47 +0300

    fix: expected behaviour

--- src/lib/binary/buffer-from-number.ts
+++ src/lib/binary/buffer-from-number.ts
@@ -2,14 +2,29 @@ import { Buffer } from "buffer";
 import { mutateOr } from "./buffer-bitwise/or";
 
 /** Source <https://stackoverflow.com/a/65227338> */
-export function bufferFromNumber  (n: number, len = 1): Buffer {
-    let buf = Buffer.alloc(len);
+export function bufferFromNumber(n: number, len?: number): Buffer {
+    let l = len || 1;
+    let buf = Buffer.alloc(l);
     if (!n) return buf;
+
     const a = []
     a.unshift(n & 255)
     while (n >= 256) {
         n = n >>> 8
         a.unshift(n & 255)
     }
-    return mutateOr(buf, Buffer.from(a));
+
+    let aBuf = Buffer.from(a);
+
+    let diff = buf.length - aBuf.length;
+
+    if (diff < 0) {
+        if (typeof len == "number") {
+            console.warn(n + ": does not fit in specified size")
+            return buf;
+        } else return aBuf;
+    }
+
+    buf.set(aBuf, diff)
+    return buf;
 }
