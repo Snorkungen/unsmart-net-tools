874f02f7ebf405986599ba3d4557d34a6cc38f78
snorkungen <teuvo2004@gmail.com>
2023-10-17 10:17:10 +0300

    bored: 8% performance increase

--- src/lib/binary/struct/shared.ts
+++ src/lib/binary/struct/shared.ts
@@ -1,9 +1,9 @@
 /** Makes buffer to a `number` */
 export function _bufToNumber(buf: Uint8Array) {
-    let n = 0, i = buf.byteLength;
-    while (i > 0) {
+    let n = 0, i = buf.byteLength, j = i -1;
+        while (i > 0) {
         // n += buf[--i] << (i * 8) // little endian
-        n += buf[--i] << ((buf.byteLength - 1 - i) * 8) // big endian
+        n += buf[--i] << ((j - i) * 8) // big endian
     }
 
     return n;
