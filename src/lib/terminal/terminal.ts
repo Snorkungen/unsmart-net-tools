2ed5c5de25fb5249d9004c5eed78713284e3c0bf
snorkungen <teuvo2004@gamil.com>
2023-10-27 23:12:03 +0300

    feate: lib/terminal "control sequences"

--- src/lib/terminal/terminal.ts
+++ src/lib/terminal/terminal.ts
@@ -1,4 +1,20 @@
 
+export enum ASCIICodes {
+    NUL = 0,
+    BackSpace = 0x08,
+    Tab = 0x09,
+    NewLine = 0x0D,
+    CarriageReturn = 0x0D,
+    Escape = 0x1B,
+    Semicolon = 0x3B,
+    OpenSquareBracket = 0x5B,
+
+
+    A = 0x41, // 65
+    Zero = 0x30, // 48
+}
+
+
 export default class Terminal {
 
 
@@ -13,12 +29,12 @@ type TerminalRendererCursor = {
 
 export class TerminalRenderer {
     // options start
-    COLUMN_WIDTH = 2 * 8;
-    ROW_HEIGHT = 2 * 8;
+    COLUMN_WIDTH = 4 * 8;
+    ROW_HEIGHT = 6 * 8;
 
 
     COLORS = [
-        "#000000",
+        "#080808",
         "#ffffff"
     ]
     COLOR_BG = 0 % this.COLORS.length;
@@ -59,6 +75,126 @@ export class TerminalRenderer {
         y: 0
     }
 
+    private handleEscapeSequences(i: number): number {
+        let byte = this.buffer[i];
+        if (byte == ASCIICodes.OpenSquareBracket) {
+            // for now only handle cursor movement
+            let rawParams: number[] = [];
+            while (++i < this.buffer.byteLength) {
+                byte = this.buffer[i];
+                if (
+                    byte >= 0x30 &&
+                    byte <= 0x3f
+                ) {
+                    rawParams.push(byte);
+                } else if (
+                    byte >= 0x40 &&
+                    byte <= 0x7E
+                ) {
+                    rawParams.push(byte);
+                    break;
+                } else {
+                    return -1; // Something unexpected happened
+                }
+            }
+
+            if (rawParams.length == 0) {
+                return -1; // error
+            }
+
+            let finalByte = rawParams[rawParams.length - 1]; rawParams.pop();
+
+            switch (finalByte) {
+                case ASCIICodes.A: {
+                    // handle Cursor up
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+
+                    this.cursor.y = Math.max(
+                        this.cursor.y - params[0],
+                        0
+                    )
+
+                    break;
+                }
+                case ASCIICodes.A + 1: { // B
+                    // handle Cursor down
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+                    this.cursor.y += params[0];
+                    break;
+                }
+                case ASCIICodes.A + 2: { // C
+                    // handle Cursor forward
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+                    this.cursor.x = Math.min(this.cursor.x + params[0], this.COLUMN_WIDTH)
+                    break;
+                }
+                case ASCIICodes.A + 3: { // D
+                    // handle Cursor Back
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+
+                    this.cursor.x = Math.max(this.cursor.x - params[0], 0)
+                    break;
+                }
+                case ASCIICodes.A + 4: { // E
+                    // handle Cursor Next Line
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+                    this.cursor.y += params[0];
+                    this.cursor.x = 0;
+                    break;
+                }
+                case ASCIICodes.A + 5: { // F
+                    // handle Cursor Previous Line
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+
+                    this.cursor.y = Math.max(
+                        this.cursor.y - params[0],
+                        0
+                    )
+                    this.cursor.x = 0;
+                    break;
+                }
+                case ASCIICodes.A + 6: { // G
+                    // handle Cursor Horizontal Absolute
+                    let params = readParams(rawParams, 1)
+                    if (params.length < 1) {
+                        return -1;
+                    }
+
+                    this.cursor.x = Math.min(
+                        params[0],
+                        this.COLUMN_WIDTH
+                    )
+
+                    break;
+                }
+
+                default: return -1; // unhandled control sequence
+            }
+
+            return i;
+        }
+
+        return -1
+    }
+
     render() {
         if (!this.container) {
             throw new Error("container missing can't render")
@@ -70,8 +206,8 @@ export class TerminalRenderer {
 
             // INSPIRATION <https://en.wikipedia.org/wiki/ANSI_escape_code>
             switch (byte) {
-                case 0x00: i++; continue char_parse_loop;
-                case 0x08: {
+                case ASCIICodes.NUL: i++; continue char_parse_loop;
+                case ASCIICodes.BackSpace: {
                     // handle backspace
                     this.cursor.x -= 1
 
@@ -88,7 +224,7 @@ export class TerminalRenderer {
                     activeElement.innerHTML = this.EMPTY_CHAR
                     break;
                 }
-                case 0x09: {
+                case ASCIICodes.Tab: {
                     // handle Tab
                     this.cursor.x += 8 - this.cursor.x % 8;
                     if (this.cursor.x >= this.COLUMN_WIDTH) {
@@ -96,18 +232,32 @@ export class TerminalRenderer {
                         this.cursor.y += 1;
                     }
                     break;
-                } case 0x0A: {
+                } case ASCIICodes.NewLine: {
                     // handle new line
                     this.cursor.x = 0;
                     this.cursor.y += 1
                     break;
                 }
-                case 0x0D: {
+                case ASCIICodes.CarriageReturn: {
                     // handle carriage return
                     this.cursor.x = 0;
+                    break;
                 }
-                case 0x1b: {
-                    // handle escape code
+                case ASCIICodes.Escape: {
+                    // move on to next byte
+                    // increment index
+                    i += 1; if (i > this.buffer.byteLength) {
+                        return;
+                    }
+
+                    let tmp = i;
+                    i = this.handleEscapeSequences(i);
+                    if (i < 0) {
+                        // something failed continue
+                        i = tmp;
+                        continue char_parse_loop;
+                    }
+
                     break
                 }
             }
@@ -129,12 +279,11 @@ export class TerminalRenderer {
             this.cursor.x += 1;
             if (this.cursor.x >= this.COLUMN_WIDTH) {
                 this.cursor.y += 1;
+                this.cursor.x = 0;
             }
 
             i++; continue char_parse_loop;
         }
-
-
 
         // clear previous cursor
         let cursorElement = this.container.children[this.prevCursor.y].children[this.prevCursor.x] as HTMLElement;
@@ -145,7 +294,6 @@ export class TerminalRenderer {
         cursorElement = this.container.children[this.cursor.y].children[this.cursor.x] as HTMLElement;
         cursorElement.style.backgroundColor = this.color(this.COLOR_FG)
         cursorElement.style.color = this.color(this.COLOR_BG)
-
         this.prevCursor.x = this.cursor.x;
         this.prevCursor.y = this.cursor.y;
 
@@ -156,4 +304,48 @@ export class TerminalRenderer {
     private color(n: number) {
         return this.COLORS[n % this.COLORS.length];
     }
+}
+
+function readParams(params: number[], fallback: number): number[] {
+    if (params.length == 0) {
+        return [fallback]
+    }
+
+    let result: number[] = [], numBuffer: number[] = [];
+    let j = 0;
+
+    let consumeNumBuffer = () => {
+        // consumeNumBuffer implementation taken from <https://www.geeksforgeeks.org/c-program-to-write-your-own-atoi/>
+        let n = 0;
+        for (let k = 0; k < numBuffer.length; k++) {
+            n = n * 10 + numBuffer[k] - ASCIICodes.Zero
+        }
+
+        numBuffer = [];
+        return n
+    }
+    while (j < params.length) {
+        let pb = params[j]
+        if (pb == ASCIICodes.Semicolon) {
+            // read number buffer
+            if (numBuffer.length == 0) {
+                result.push(fallback)
+            } else {
+                result.push(consumeNumBuffer())
+            }
+            j++;
+            continue;
+        }
+
+        if (pb >= ASCIICodes.Zero && pb < ASCIICodes.Zero + 10) {
+            numBuffer.push(pb)
+        }
+
+        j++;
+    }
+
+    if (numBuffer.length > 0) {
+        result.push(consumeNumBuffer())
+    }
+    return result;
 }
--- src/views/testing-component2.tsx
+++ src/views/testing-component2.tsx
@@ -1,29 +1,56 @@
 import { Component, createEffect } from "solid-js";
-import { TerminalRenderer } from "../lib/terminal/terminal";
-import { uint8_fromString } from "../lib/binary/uint8-array";
+import { ASCIICodes, TerminalRenderer } from "../lib/terminal/terminal";
+import { uint8_concat, uint8_fromString } from "../lib/binary/uint8-array";
 
 
 export const TestingComponent2: Component = () => {
 
     let terminalRenderer: TerminalRenderer;
 
-    let bytes = uint8_fromString("A\tB\tC\bD\nE\tF \b\rG\n")
+    // let bytes = uint8_fromString("A\tB\tC\bD\nE\tF \b\rG\n")
 
 
-    function fooAnimate(bi = 0): void {
-        if (bi > bytes.byteLength) {
-            return;
-        }
+    // function fooAnimate(bi = 0): void {
+    //     if (bi > bytes.byteLength) {
+    //         return;
+    //     }
 
 
-        terminalRenderer.buffer = bytes.slice(bi, bi + 1)
-        terminalRenderer.render();
+    //     terminalRenderer.buffer = bytes.slice(bi, bi + 1)
+    //     terminalRenderer.render();
 
-        window.setTimeout(() => fooAnimate(bi + 1), 1000)
+    //     window.setTimeout(() => fooAnimate(bi + 1), 1000)
+    // }
+
+    // createEffect(() => fooAnimate())
+
+    function sescape(str: string): Uint8Array {
+        return uint8_concat([
+            new Uint8Array([ASCIICodes.Escape]),
+            uint8_fromString(str),
+        ])
     }
 
-    createEffect(() => fooAnimate())
+    createEffect(() => {
+        terminalRenderer.buffer = uint8_concat([
+            sescape("[;B"),
+            uint8_fromString("Hello World"),
+            sescape("[;B"),
+            sescape("[;A"),
+            sescape("[;C"),
+            sescape("[;D"),
+            uint8_fromString("Hello World"),
+            sescape("[2E"),
+            uint8_fromString("Hello World"),
+            sescape("[F"),
+            uint8_fromString("Hello World"),
+            sescape("[1B"),
+            sescape("[4G"),
+            uint8_fromString("_______"),
+        ])
+        terminalRenderer.render();
 
+    })
 
     return (
         <div>
