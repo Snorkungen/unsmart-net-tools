3e16f55925de2c49b1cabe2763d2d3c71837d197
snorkungen <teuvo2004@gmail.com>
2024-06-10 09:51:57 +0300

    refactor: struct-viewer.tsx

--- src/views/struct-viewer.tsx
+++ src/views/struct-viewer.tsx
@@ -1,12 +1,14 @@
-import { Component, For } from "solid-js"
-import { IPV4_HEADER } from "../lib/header/ip"
-import { BYTE_ARRAY, SLICE, Struct, StructOptions, StructType, UINT16, UINT32, UINT8 } from "../lib/binary"
+import { Accessor, Component, Setter, createEffect, createMemo, createSignal } from "solid-js"
+import { type StructType, type Struct } from "../lib/binary"
 import { UDP_HEADER } from "../lib/header/udp";
-import { DHCP_HEADER } from "../lib/header/dhcp/dhcp";
+
+type AnyStruct = Struct<any>;
+
+const TABLE_WIDTH = 32; // 4-Bytes
 
 /** just can't be bothered to do anything else */
 function struct_get_types<T extends Record<string, StructType<any>>>(struct: Struct<T>): T {
-    // @ts-ignore
+    // @ts-expect-error
     return struct.types;
 }
 
@@ -16,13 +18,7 @@ const truncateString = (string = '', maxLength = 50) =>
         ? `${string.substring(0, maxLength)}…`
         : string
 
-type JoinedStructTypes = {
-    bitLength: number;
-    names: (number | Symbol | string)[]
-    struct_types: StructType<any>[]
-}
-
-const StructViewerComponent: Component<{ struct: Struct<any> }> = ({ struct }) => {
+const StructViewerComponent: Component<{ struct: AnyStruct, active_value_idx: Accessor<number>, set_active_value_idx: Setter<number> }> = ({ struct, active_value_idx, set_active_value_idx }) => {
     const struct_order = [...struct.order];
     // @ts-expect-error
     const struct_types = struct.types as Record<(string | number | symbol), StructType<any>>;
@@ -31,60 +27,18 @@ const StructViewerComponent: Component<{ struct: Struct<any> }> = ({ struct }) =
     // !TODO: join chunks that are smaller than 8-bits into single chunk so it can be handled as a special case
     let ordered_struct_types = struct_order.map((key) => struct_types[key])
 
-    const joined_struct_types_name = "__joined__struct_types"
-
-    let joined_struct_types: Record<number, { names: (string | number | symbol)[]; struct_types: StructType<any>[]; bitLength: number }> = {}
-
-
-
-    for (let i = 0; i < struct_order.length; i++) {
-        let st = ordered_struct_types[i];
-
-        // the field is not on a an 8-bit boundary
-        if (st.bitLength % 8 > 0) {
-            // assume that the total length is on an 8-bit boundary
-            let bl = st.bitLength;
-            for (let j = i + 1; j < struct_order.length; j++) {
-                let sub_st = ordered_struct_types[j]
-                bl += sub_st.bitLength;
-
-                if (bl % 8 == 0) {
-                    // wrap the given values and struct types into a single object
-                    // and remove the some stuff from the array
-                    // console.log(struct_order.slice(i, j + 1))
-
-                    let names = struct_order.splice(i, j - i + 1, joined_struct_types_name)
-                    let struct_types = ordered_struct_types.splice(i, j - i + 1, {
-                        bitLength: bl,
-                        getter() { throw Error() },
-                        setter() { throw Error() }
-                    })
-
-                    joined_struct_types[i] = {
-                        names,
-                        struct_types,
-                        bitLength: bl
-                    }
-
-                    i -= j - i;
-                    break
-                }
-            }
-        }
-    }
-
     function get_width_and_height(struct_type: StructType<any>) {
         let height = 1;
-        let width = Math.ceil(struct_type.bitLength / 8)
-        if (width > 4) {
-            height = Math.ceil(width / 4)
+        let width = struct_type.bitLength
+        if (width > TABLE_WIDTH) {
+            height = Math.ceil(width / TABLE_WIDTH)
             height = Math.min(height, 4) // cap the height to make it easier to look at
-            width = 4
+            width = TABLE_WIDTH
             // do something 
             // modify the height
         } else if (width <= 0) {
             height = 4;
-            width = 4;
+            width = TABLE_WIDTH;
         }
 
         return [width, height]
@@ -101,7 +55,7 @@ const StructViewerComponent: Component<{ struct: Struct<any> }> = ({ struct }) =
         let row_start = row_end;
 
         col_end += w;
-        if (col_end > 4) {
+        if (col_end > TABLE_WIDTH) {
             col_end = 1;
             row_end += h
         }
@@ -120,79 +74,76 @@ const StructViewerComponent: Component<{ struct: Struct<any> }> = ({ struct }) =
 
     return <div style={{
         display: "grid",
-        "grid-template-columns": "1fr 1fr 1fr 1fr",
+        "grid-template-columns": "repeat(" + TABLE_WIDTH + ", 1fr)",
         "grid-template-rows": "repeat(" + row_count + ", 1fr)",
     }}>{struct_order.map((name, i) => {
-        let { col_start, col_end, row_start, row_end, width, height } = precomputed_sizes[i]
+        let { col_start, col_end, row_start, row_end } = precomputed_sizes[i]
 
         let content;
-        if (name === joined_struct_types_name) {
-            let jst = joined_struct_types[i];
-            
-            content = <div style={{
-                display: "grid",
-                width: "100%",
-                "grid-template-columns": "repeat(" + jst.bitLength + ", 1fr)",
-            }}>{
 
-                    jst.names.map((name, j) => {
-                        let st = jst.struct_types[j]
-                        let col_start = jst.struct_types.slice(0, j).reduce((sum, { bitLength }) => sum + bitLength, 0) + 1;
-                        let col_end = col_start + st.bitLength;
-                        return <div style={{
-                            "grid-column-start": col_start,
-                            "grid-column-end": col_end,
+        content = truncateString(
+            `"${name.toString()}": ${struct.get(name)}`,
+            30 // !TODO: compute how many characters can fit into the given field
+        )
 
-                            "border-left": j == 0 ? "none" : "solid 2px #010101",
-                            "text-align": "center"
-                        }}>
-                            {name.toString()}
-                        </div>
-                    })
-                }</div>
-        } else {
+        return <div
+            style={{
+                "grid-column-start": col_start,
+                "grid-column-end": col_end,
+                "grid-row-start": row_start,
+                "grid-row-end": row_end,
 
-            content = truncateString(
-                `"${name.toString()}": ${struct.get(name)}`,
-                30 // !TODO: compute how many characters can fit into the given field
-            )
-        }
+                "display": "flex",
+                "align-items": "center",
+                "justify-content": "center",
+                "text-align": "center",
 
+                "padding": "0.4em",
+                "border": "solid 2px #e1e1e1",
+                "border-top": row_start == 1 ? "inherit inherit inherit" : "none",
+                "border-left": col_start == 1 ? "inherit inherit inherit" : "none",
 
-        return <div style={{
-            "grid-column-start": col_start,
-            "grid-column-end": col_end,
-            "grid-row-start": row_start,
-            "grid-row-end": row_end,
-
-            "display": "flex",
-            "align-items": "center",
-            "justify-content": "center",
-            "text-align": "center",
-
-            "padding": "0",
-            "border": "solid 2px #010101",
-            "border-top": row_start == 1 ? "solid 2px #010101" : "none",
-            "border-left": col_start == 1 ? "solid 2px #010101" : "none"
-        }}>{content}</div>
+                "opacity": active_value_idx() == i ? "0.8" : undefined,
+                "border-bottom-color": active_value_idx() == i ? "#000" : undefined
+            }}
+            onclick={() => set_active_value_idx(i)}
+        >{content}</div>
     })}
     </div>
 }
 
 export const StructViewer: Component = () => {
-    let struct = new Struct({
-        "test1": UINT8,
-        "test2": UINT16,
-        "test3": UINT8,
-        "test4": UINT32,
-        "test5": BYTE_ARRAY(8),
-        "payload": SLICE
-    })
-    let types = struct_get_types(struct)
+
+
+    let struct: AnyStruct = UDP_HEADER.create({
+        sport: 9023,
+        dport: 7000,
+        length: 30,
+        payload: new Uint8Array({ length: 30 - UDP_HEADER.getMinSize() })
+    });
+
+    // The struct is known, so the only thing that is needed is the idx, invalid idx means that it is the struct
+    const [active_value_idx, set_active_value_idx] = createSignal<number>(-1);
+    const active_value = createMemo(() => {
+        if (active_value_idx() >= 0 && active_value_idx() < struct.order.length) {
+            let key = struct.order[active_value_idx()]
+            return {
+                key: key,
+                // @ts-expect-error
+                offset: struct.getTypeBitOffset(),
+                struct_type: struct_get_types(struct)[key]
+            }
+        }
+
+        return struct;
+    }, struct)
+
+    // !TODO: someway to inspect value, and bit information
 
     return <div>
-        {/* <StructViewerComponent struct={struct} ></StructViewerComponent>
-        <StructViewerComponent struct={UDP_HEADER} ></StructViewerComponent> */}
-        <StructViewerComponent struct={IPV4_HEADER} />
+        <StructViewerComponent struct={struct} active_value_idx={active_value_idx} set_active_value_idx={set_active_value_idx} />
+        <div>
+            Here we show information about the selected item
+        </div>
     </div>
 }
