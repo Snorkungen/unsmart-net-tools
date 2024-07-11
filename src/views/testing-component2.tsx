5e403e720ae22d5759eac7ce749f9201c1e85f2e
snorkungen <teuvo2004@gmail.com>
2024-07-11 11:30:47 +0300

    update: testing-component2.tsx

--- src/views/testing-component2.tsx
+++ src/views/testing-component2.tsx
@@ -42,8 +42,9 @@ export const TestingComponent2: Component = () => {
     newdevice.db_set("test", "hello world")
 
     let osif = newdevice.interface_add(new OSInterface(newdevice))
-
-    newdevice.interface_set_address(osif, new IPV4Address("10.1.1.10"), createMask(IPV4Address, 16))
+    const osif_destination = new IPV4Address("10.1.1.40");
+    const osif_dport = 10011;
+    newdevice.interface_set_address(osif, new IPV4Address("10.1.1.100"), createMask(IPV4Address, 16))
 
     function send_ping() {
         function success() {
@@ -55,10 +56,9 @@ export const TestingComponent2: Component = () => {
             id: identifier,
             seq: sequence
         })
-        let ip = new IPV4Address("10.1.1.40")
+        let ip = osif_destination;
         let destination = new IPV4Address(ip)
 
-        
         let contact = newdevice.contact_create("IPv4", "RAW").data!;
         contact.receive(contact, (_, data) => {
             let iphdr = IPV4_HEADER.from(data.buffer);
@@ -68,28 +68,48 @@ export const TestingComponent2: Component = () => {
             contact.close(contact);
             success()
         })
-        
+
         let icmpHdr = ICMP_HEADER.create({
             type: ICMPV4_TYPES.ECHO_REQUEST,
             data: echoHdr.getBuffer()
         });
-        
+
         icmpHdr.set("csum", calculateChecksum(icmpHdr.getBuffer()));
-        
+
         let ipHdr = createIPV4Header({
             saddr: new IPV4Address("0.0.0.0"),
             daddr: new IPV4Address(ip),
             proto: PROTOCOLS.ICMP,
             payload: icmpHdr.getBuffer()
         })
-        
+
         contact.send(contact, { buffer: ipHdr.getBuffer() }, ipHdr.get("daddr"));
+    }
+
+    function send_udp() {
+        let contact = newdevice.contact_create("IPv4", "UDP").data!;
+
+        contact.sendTo(contact, {
+            buffer: new Uint8Array([
+                0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x55, 0x44, 0x50, 0x20, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72
+            ])
+
+        }, {
+            dport: osif_dport,
+            daddr: osif_destination
+        });
+
+        contact.receive(contact, (_, d) => {
+            let string = new TextDecoder("utf-8").decode(d.buffer);
+            console.log(string);
+        }, {})
     }
 
     return (
         <div>
             <button onclick={() => osif.start()}>start osif</button>
             <button onclick={send_ping}>send packet over wire</button>
+            <button onclick={send_udp}>send udp packet over wire</button>
             <div ref={(el) => {
                 terminal = new Terminal(el)
             }}></div>
