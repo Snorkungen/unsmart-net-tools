b32a6c464183694c7e97ca993f365ce3d9bb7a7c
snorkungen <teuvo2004@gmail.com>
2024-09-25 14:18:10 +0300

    update: device/network-switch.ts

--- src/lib/device/network-switch.ts
+++ src/lib/device/network-switch.ts
@@ -3,27 +3,27 @@ import { ETHERNET_HEADER } from "../header/ethernet";
 import { Device, ProcessSignal, Program } from "./device";
 import { BaseInterface, EthernetInterface } from "./interface";
 
-
-export class NetworkSwitch extends Device {
-    constructor() {
-        super();
-        this.process_start(testing_switch_stuff);
-    }
-
-    interface_add<F extends BaseInterface>(iface: F): F {
-        if (iface instanceof EthernetInterface) {
-            iface.vlan_set("access", 1); // initialize vlans so that they all get a vlan
-        }
-
-        return super.interface_add(iface);
-    }
+/** attributes and data needed for the ethernet bridging ... */
+export type NetworkSwitchStoreData = {
+    ports: Record<string, {
+        // TODO: store information relating to a specific port
+    }>;
+    /** K: MACAddress.toString(), V: Outgoing port */
+    macaddresses: Map<string, EthernetInterface>;
 }
 
-const testing_switch_stuff: Program = {
-    name: "testing_switch_stuff",
+export const DAEMON_NETWORK_SWITCH_STORE_KEY = "network_switch_data";
+const DAEMON_NETWORK_SWITCH: Program = {
+    name: "daemon_network_switch",
+    description: "receive and forward ethernet frames",
     init(proc) {
+        // NOTE: this program assumes that the store object does not get changed
+        const store = proc.device.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY) as NetworkSwitchStoreData;
+        if (!store) {
+            throw new Error("network switch store is not defined");
+        }
+
         let contact = proc.device.contact_create("RAW", "RAW").data!;
-        let macaddresses = new Map<string, EthernetInterface>();
 
         contact.receive(contact, (_, data) => {
             if (!(data.rcvif instanceof EthernetInterface)) return;
@@ -34,6 +34,9 @@ const testing_switch_stuff: Program = {
             if (is_unicast && data.destination) {
                 return; // do not forward packet is for host and host only
             }
+
+            let rcvif_info = store.ports[data.rcvif.id()];
+            if (!rcvif_info) throw new Error("port must be configured");
 
             function forward(iface: BaseInterface) {
                 iface.output({
@@ -50,13 +53,13 @@ const testing_switch_stuff: Program = {
                 }
             }
 
-            macaddresses.set(etherheader.get("smac").toString(), data.rcvif);
+            store.macaddresses.set(etherheader.get("smac").toString(), data.rcvif);
 
             if (data.broadcast || etherheader.get("dmac").isBroadcast()) {
                 return flood()
             }
 
-            let iface = macaddresses.get(etherheader.get("dmac").toString());
+            let iface = store.macaddresses.get(etherheader.get("dmac").toString());
             if (!iface) {
                 return flood()
             }
@@ -70,3 +73,33 @@ const testing_switch_stuff: Program = {
     },
     __NODATA__: true
 }
+
+export class NetworkSwitch extends Device {
+    switch_data: NetworkSwitchStoreData;
+    constructor() {
+        super();
+
+        // set the network swithc store data
+        this.switch_data = <NetworkSwitchStoreData>{
+            macaddresses: new Map(),
+            ports: {},
+        }
+        this.store.set(DAEMON_NETWORK_SWITCH_STORE_KEY, this.switch_data);
+
+        this.process_start(DAEMON_NETWORK_SWITCH);
+    }
+
+    interface_add<F extends BaseInterface>(iface: F): F {
+        if (iface instanceof EthernetInterface) {
+            iface.vlan_set("access", 1); // initialize vlans so that they all get a vlan
+        }
+
+        // add a default port configureation for the given interfce
+        (this.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY)! as NetworkSwitchStoreData).ports[iface.id()] = {
+            // !TODO: add default port configureation
+        }
+
+        return super.interface_add(iface);
+    }
+}
+
