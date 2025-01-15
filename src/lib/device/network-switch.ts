e52de130e52bd129c472f5d415ea95eb18469c11
snorkungen <teuvo2004@gmail.com>
2025-01-15 15:01:58 +0200

    update: device/network-switch.ts

--- src/lib/device/network-switch.ts
+++ src/lib/device/network-switch.ts
@@ -15,10 +15,38 @@ export enum NetworkSwitchPortState {
 /** attributes and data needed for the ethernet bridging ... */
 export type NetworkSwitchStoreData = {
     ports: Record<string, {
-        state: NetworkSwitchPortState
+        state: NetworkSwitchPortState,
+        priority: number;
+
+        path_cost: number;
+
     }>;
     /** K: MACAddress.toString(), V: Outgoing port */
     macaddresses: Map<string, EthernetInterface>;
+
+
+    stp_enabled?: 1; /** truthy value wether if stp is enabled */
+    stp_params?: {
+        /** designated root */
+        root_id: Uint8Array; // actually 64 bit integer but who cares
+        root_path_cost: number;
+        root_port: string;
+
+        max_age: number,
+        hello_time: number;
+        forward_delay: number;
+
+        bridge_id: Uint8Array;
+
+        bridge_max_age: number;
+        bridge_hello_time: number;
+        bridge_forward_delay: number;
+
+        tcn_detected: boolean;
+        topology_change: boolean;
+
+        hold_time: 1; // fixed value set to 1
+    }
 }
 
 export const DAEMON_NETWORK_SWITCH_STORE_KEY = "network_switch_data";
@@ -126,19 +154,70 @@ export class NetworkSwitch extends Device {
     }
 
     interface_add<F extends BaseInterface>(iface: F): F {
+        iface = super.interface_add(iface);
+
         if (iface instanceof EthernetInterface) {
             iface.vlan_set("access", 1); // initialize vlans so that they all get a vlan
+
+            this.interface_mcast_subscribe(iface, new MACAddress("01-80-C2-00-00-00"));
         }
 
-        let stp_enabled = false; // assume STP is disabled by default
+        let stp_enabled = (this.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY)! as NetworkSwitchStoreData).stp_enabled;
 
         // add a default port configureation for the given interfce
         (this.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY)! as NetworkSwitchStoreData).ports[iface.id()] = {
-            state: stp_enabled ? NetworkSwitchPortState.BLOCKING : NetworkSwitchPortState.FORWARDING
+            state: !!stp_enabled ? NetworkSwitchPortState.BLOCKING : NetworkSwitchPortState.FORWARDING,
+
+            priority: 0x8000, // default (32768)
+            path_cost: 0
             // !TODO: add default port configuration
         }
 
-        return super.interface_add(iface);
+        return iface;
+    }
+
+    /** enable stp for the device & stuff */
+    stp_enable() {
+        // sanity check
+        if (!this.interfaces.some((iface) => iface instanceof EthernetInterface)) {
+            throw "cannot enable stp"
+        }
+
+        // setup the parameters
+        if (!this.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY)) throw "something has gone wrong"
+
+
+        this.switch_data = this.store.get(DAEMON_NETWORK_SWITCH_STORE_KEY) as NetworkSwitchStoreData;
+        this.switch_data.stp_enabled = 1;
+
+        if (this.switch_data.stp_enabled) return;
+
+        let bridge_id = new Uint8Array(8);
+        let root_port = ""; // this what is the default no configured value?
+
+        // !TODO: consult the standard for the real numbers
+        let max_age = 0, hello_time = 0, forward_delay = 0;
+
+        // !TODO: handle existing parameters
+        this.switch_data.stp_params = {
+            bridge_id: bridge_id,
+            root_id: bridge_id,
+            root_path_cost: 0,
+            root_port: root_port,
+
+            max_age: max_age,
+            hello_time: hello_time,
+            forward_delay: forward_delay,
+            bridge_max_age: max_age,
+            bridge_hello_time: hello_time,
+            bridge_forward_delay: forward_delay,
+
+            tcn_detected: false,
+            topology_change: false,
+
+            hold_time: 1, // fixed value set to 1
+        }
+
     }
 }
 
---
+++ src/lib/header/bpdu.ts
@@ -0,0 +1,29 @@
+import { BYTE_ARRAY, defineStruct, UINT16, UINT32, UINT8 } from "../binary";
+
+/** Topology Change Notification BPDU */
+export const BPDU_TCN_HEADER = defineStruct({
+    /* protocol ID (0x0000) */
+    "proto_id": UINT16,
+    /** version ID (0x00) */
+    "version_id": UINT8,
+    /** type (0x80) */
+    "type": UINT8,
+});
+
+/** Configuration BPDU */
+export const BPDU_C_HEADER = defineStruct({
+    /* protocol ID (0x0000) */
+    "proto_id": UINT16,
+    "version_id": UINT8,
+    "type": UINT8,
+    "flags": UINT8,
+    "root_id": BYTE_ARRAY(8),
+    "root_path_cost": UINT32,
+    "bridge_id": BYTE_ARRAY(8),
+    "port_id": UINT32,
+    "message_age": UINT32,
+    "max_age": UINT32,
+    "hello_time": UINT32,
+    "forward_delay": UINT32,
+    "v1_length": UINT8,
+});
