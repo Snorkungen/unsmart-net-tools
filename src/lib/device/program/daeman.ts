fb568b53af1ef6eb9ac7305141a89c737cdbd6b5
snorkungen <teuvo2004@gmail.com>
2025-06-22 18:59:20 +0300

    fix: device/program/daeman.ts

--- src/lib/device/program/daeman.ts
+++ src/lib/device/program/daeman.ts
@@ -1,34 +1,53 @@
-/* DAEMAN MANAGER */
+import { ProcessSignal, Program } from "../device";
+import { PPFactory, ProgramParameterDefinition } from "../internals/program-parameters";
+import { formatTable, ioprintln } from "./helpers";
 
-/* Maybe move away from daemon terminology due to them just being headless programs ... */
-
-/* Should this program be called procman or daemon,
-    because this thing manages daemons ... 
-    but there is no way to during runtime to determine if a program is a daemon other than the name being dameon_*
-    but that should only be for the human readability purposes */
-
-import { uint8_fromString } from "../../binary/uint8-array";
-import { Process, ProcessSignal, Program } from "../device";
-
-/* Rev 1. list all the running programs */
+const pdef = new ProgramParameterDefinition([
+    ["daeman",],
+    ["daeman", PPFactory.number("PID"), PPFactory.keywords("ACTION", ["interrupt", "close"])]
+])
 
 export const DEVICE_PROGRAM_DAEMAN: Program = {
     name: "daeman",
-    init: function (proc: Process<any>, args: string[], data?: Partial<any> | undefined): ProcessSignal {
-        let i = 0;
-        /* write out line-by-line each process */
-        for (let p of proc.device.processes.items) {
-            if (!p?.id || p == proc) continue;
+    parameters: pdef,
+    init(proc, sargs) {
+        const res = pdef.parse(proc.device, sargs);
 
-            // @ts-expect-error
-            let jentries = proc.device.process_journal_entries.get(p.id);
-            if (jentries) {
-                console.log(p.id, jentries)
-            }
-
-            proc.io.write(uint8_fromString(`${i++}:\t${p.id}\t${p.status}\n`));
+        if (!res.success) {
+            ioprintln(proc.io, pdef.message(res));
+            return ProcessSignal.ERROR;
         }
 
+        const processes = proc.device.processes;
+        const [, pid, action] = res.arguments;
+
+        if (typeof pid === "number") {
+            let daemon = processes.items[pid];
+
+            if (!daemon) {
+                ioprintln(proc.io, "process not found");
+                return ProcessSignal.ERROR;
+            }
+
+            if (action == "close") {
+                daemon.close(ProcessSignal.EXIT);
+            } else if (action == "interrupt") {
+                daemon.close(ProcessSignal.INTERRUPT);
+            }
+        }
+
+        let table: string[][] = [["PID", "name", "status"]];
+        for (let i = 0; i < processes.items.length; i++) {
+            let daemon = processes.items[i];
+
+            if (!daemon || daemon === proc) {
+                continue;
+            }
+
+            table.push([i + ":", daemon.id, daemon.status]);
+        }
+
+        proc.io.write(formatTable(table));
 
         return ProcessSignal.EXIT;
     },
