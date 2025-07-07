84faae3f64a0cb1738f84a4415a48c356776521f
snorkungen <teuvo2004@gmail.com>
2025-07-07 10:40:57 +0300

    update: device/internals/program.ts

--- src/lib/device/internals/program.ts
+++ src/lib/device/internals/program.ts
@@ -1,11 +1,17 @@
-import { Program } from "../device";
+import type { Program } from "../device";
 
 export const ALL_REGISTERED_PROGRAMS: Program[] = [];
 export function device_program_register<T>(program: Program<T>): Program<T> {
+
     // de-dupe only a problem when hot-reloading
-    if (true) {
-        if (ALL_REGISTERED_PROGRAMS.some(p => p.name === program.name)) return program;
+    if (import.meta.env.MODE == "development") {
+        let i = ALL_REGISTERED_PROGRAMS.findIndex((p) => p.name == program.name);
+        if (i >= 0) {
+            ALL_REGISTERED_PROGRAMS[i] = program;
+            return program;
+        }
     }
+
     ALL_REGISTERED_PROGRAMS.push(program);
     return program;
 }
